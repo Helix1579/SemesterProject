fb834da32f496a31b3cc63a9d62c14c043059e51
Helix1579 <mashruwalayashraj9@gmail.com>
2022-05-24 16:40:17 +0200

    commit

---
+++ src/main/OverLookHotel.java
@@ -0,0 +1,12 @@
+package main;
+
+import javafx.application.Application;
+import view.Start;
+
+public class OverLookHotel
+{
+  public static void main(String[] args)
+  {
+    Application.launch(Start.class);
+  }
+}
---
+++ src/view/Start.java
@@ -0,0 +1,20 @@
+package view;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Start extends Application
+{
+  @Override public void start(Stage stage) throws Exception
+  {
+    stage.setTitle("The Overlook Hotel");
+    FXMLLoader loader = new FXMLLoader();
+    loader.setLocation(getClass().getResource("view.Homepage.fxml"));
+    Scene scene = new Scene(loader.load());
+    stage.setScene(scene);
+    stage.show();
+  }
+}
