dc57e8ab0067068fa1a7cd3cc53f8493b9abc417
Nero <Ivan.i.stefanov999:@abv.bg>
2022-05-31 15:02:19 +0100

    Ivan

--- src/view/CancelController.java
+++ src/view/CancelController.java
@@ -27,18 +27,17 @@ public class CancelController
   @FXML DatePicker CheckOutDP;
   @FXML ComboBox<Guest> GuestCB;
 
-  public void init(ViewHandler viewHandler, GuestModelManager modelManager, Region root)
+  public void init(ViewHandler viewHandler, GuestModelManager modelManager,
+      Region root)
   {
     this.modelManager = modelManager;
     this.root = root;
     this.viewHandler = viewHandler;
   }
 
-
-
-
-  public void eventHandler(ActionEvent e){
-    if(e.getSource() == ExitB)
+  public void eventHandler(ActionEvent e)
+  {
+    if (e.getSource() == ExitB)
     {
       System.exit(1);
     }
@@ -46,17 +45,15 @@ public class CancelController
     {
       viewHandler.openView("Homepage");
     }
-    else  if (e.getSource()== CancelB){
-      JOptionPane.showMessageDialog(null,"Cancel complete");
+    else if (e.getSource() == CancelB)
+    {
+      JOptionPane.showMessageDialog(null, "Cancel complete");
     }
-
 
   }
 
   public void reset()
   {
-
-
 
   }
 
@@ -85,29 +82,45 @@ public class CancelController
       GuestCB.getSelectionModel().select(index);
     }
   }
+
   public void searchHandle(ActionEvent e)
   {
-    if(e.getSource() == SearchB)
+    if (e.getSource() == SearchB)
     {
       UpdateGuestBox();
       Guest temp = GuestCB.getSelectionModel().getSelectedItem();
 
-      if(temp != null)
+      if (temp != null)
       {
         NameTF.setText(temp.getName());
         PhoneTF.setText((temp.getPhoneNumber()));
         RoomNumberTF.setText(temp.getRoomNumber());
-
+        CheckInDP.setValue(temp.getCheckInDate());
         CheckOutDP.setValue(temp.getCheckOutDate());
-
       }
     }
   }
 
   private void UpdateGuestBox()
   {
-  }
 
-  {
+    int index = GuestCB.getSelectionModel().getSelectedIndex();
+    GuestCB.getItems().clear();
+    GuestList guests = modelManager.getAllGuest();
+    for (int i = 0; i < guests.size(); i++)
+    {
+      GuestCB.getItems().add(guests.get(i));
+    }
+    if (index == -1 && GuestCB.getItems().size() > 0)
+    {
+      GuestCB.getSelectionModel().select(0);
+    }
+    else
+    {
+      GuestCB.getSelectionModel().select(index);
+
+    }
   }
 }
+
+
