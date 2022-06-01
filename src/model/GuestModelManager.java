4a8096ac099a050cc12fac162c0d9058141d4b81
Helix1579 <mashruwalayashraj9@gmail.com>
2022-06-01 14:34:24 +0200

    Yash

--- src/model/GuestModelManager.java
+++ src/model/GuestModelManager.java
@@ -129,9 +129,11 @@ public class GuestModelManager
    * to add guest to guestlist
    * @param guest the guest to replace with.
    */
-  public void addGuest(Guest guest){
+  public void addGuest(Guest guest)
+  {
     GuestList allGuests = getAllGuest();
-        allGuests .add(guest);
+    allGuests.add(guest);
+    saveGuest(allGuests);
   }
 }
 
